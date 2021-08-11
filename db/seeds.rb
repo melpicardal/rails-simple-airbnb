865076a6069ebb5be7ba5c5d7074beef678db746
meljun picardal <meljun.one@gmail.com>
2021-08-12 00:15:23 +0900

    created controlers & routes, seeded 4 flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning db"
+Flat.destroy_all
+puts "Cleaned up!"
+
+puts 'Creating Flats'
+
+4.times do
+  Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Quotes::Shakespeare,
+    price_per_night: rand(100..500),
+    number_of_guests: rand(1..5)
+  )
+end
+
+puts "Created #{Flat.count} flats"
